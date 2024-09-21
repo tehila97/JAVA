d6d06b8cb7618d1b9e7b79b3b359618ca22d01af
tehila97 <tehila284@gmail.com>
2024-09-22 00:56:17 +0300

    Add files via upload

---
+++ this_and_new.js
@@ -0,0 +1,48 @@
+// ex 1
+
+// const movieTitles = ["Incepation", "The dark knight", "Interstellar"];
+// const seats = [1, 2, 3];
+
+// movieTitles.push ("Avatar");
+
+// const updatedMovieTitles = movieTitles.filter(title => title !== "Interstellar");
+
+// console.log (updatedMovieTitles);
+
+// function MovieTheater (numbersOfSeats) {
+// this.movies = ["Incepation", "The dark knight", "Interstellar"];
+// this.seats = Array (numbersOfSeats).fill(false); 
+
+// this.addMovie = function (movie) {
+//     this.movies.push(movie);
+// };
+//  this.bookSeat = function (seatNumber) {
+
+//     if (this.seats [seatNumber]=== false) {
+//     this.seats[seatNumber] = true;
+//     console.log (`seat order ${seatNumber}`);
+//     } else {
+//         console.log (`${seatNumber} ollready taken `);
+//     }
+// };
+// }
+// const theater = new MovieTheater (100);
+
+// theater.addMovie ({title: "Avatar", duration: 162});
+// theater.bookSeat (25);
+// theater.bookSeat (100);
+
+
+// ex 2
+
+const movieTitle = "Incepation";
+const movieDuration = 128;
+const Ticket price = 30;
+
+const getInfo = [movieTitle = "Incepation", Duration = 128, price = 30 ];
+
+console.log (getInfo);
+
+
+// function Movie () {
+
