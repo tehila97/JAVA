da1ce93ef8a2cf5ac407d250654d0da526e56b21
tehila97 <tehila284@gmail.com>
2024-09-24 00:29:26 +0300

    Add files via upload

---
+++ OOP.js
@@ -0,0 +1,74 @@
+// class Media {
+//     #title;
+//     #duration;
+//     #ratings;
+
+//    constructor(title,duration) {
+//     // this.#title = "Inception";
+//     // this.#duration = 180;
+// //     // ratings = [];
+// //     this.#title = title;
+// //     this.#duration = duration;
+// //     this.#ratings = [];
+// //    } 
+
+// //     newMedia () {
+// //     return this.#title;
+// //     }
+// // };
+
+// // const media = new Media ("Inception",180);
+// // console.log (media.newMedia());  
+
+// //  1.3
+// class Media {
+//     #title;
+//     #duration;
+//     #ratings;
+
+//    constructor(title,duration) {
+//     this.#title = title;
+//     this.#duration = duration;
+//     this.#ratings = [];
+//    } 
+    
+
+//    get title (){
+//     return this.#title;
+//    }
+//    get duration () {
+//     return this.#duration;
+//    }
+// }
+
+// const media = new Media ("Inception",180);
+
+// console.log (media.title);
+// console.log (media.duration);
+
+// // // 1.4
+// class Media {
+//     #title;
+//     #duration;
+//     #ratings;
+// }
+//         constructor (title, duration) {
+//             this.#title = title;
+//             this.#duration = duration;
+//             this.#ratings = [];
+//         }  
+        
+//         addRating (rating) {
+//             this.#rating.push(rating);
+//         }
+        
+//         getRatings(){
+//             return this.#ratings;
+//         }
+        
+//         const media = new Media("Inception", 180);
+//         media.addRating(9.0);
+//         media.addRating (8.5);
+//         console.log(media.getRatings());  
+
+// 1.5
