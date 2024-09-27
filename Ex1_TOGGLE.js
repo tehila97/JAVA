3e85e6a742e14bae60de6924deb9a7ddc1bed328
tehila97 <tehila284@gmail.com>
2024-09-27 17:07:40 +0300

    Add files via upload

---
+++ Ex1_TOGGLE.js
@@ -0,0 +1,14 @@
+// ex1
+
+// document.getElementById('toggleButton').addEventListener('click', function() {
+//     document.body.classList.toggle('dark');
+// });
+
+// ex2
+
+// const toggleButton = document.getElementById('toggleButton');
+// function toggleBackgroundColor (){
+//     document.body.classList.toggle('dark');
+// }
+// toggleButton.addEventListener('click', toggleBackgroundColor);
+
