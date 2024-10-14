ff6bd45803c667b7933c71bad7dbecb146cce4f4
tehila97 <tehila284@gmail.com>
2024-10-14 16:12:35 +0300

    Add files via upload

---
+++ String_concatenation.js
@@ -0,0 +1,30 @@
+// ex 1
+
+const name = 'tehila';
+const age = 27;
+
+// // console.log (`my name is` name.concat('my name is and my age is', age))
+
+console.log (" my name is ".concat(name, " and my age is ", age));
+
+// ex 2
+
+const favMovie = "inception";
+const favColor = "blue";
+const favFood = "pasta";
+
+console.log ("my favorit color is " .concat(favColor, ", my favorite movie is '" .concat(favMovie, "' and i enjoy eating " , favFood)));
+
+// ex 3
+
+const a = 5;
+const b = 10;
+const sum = a + b;
+
+console.log ("ths sum of a and b is " .concat(sum, " where " .concat(sum, " is the sum of a and b")));
+
+// ex 4
+
+const day = "Monday";
+
+console.log ("Today is " .concat(day));
