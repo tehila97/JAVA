cd0293969f81abe0d7b93502d204b1d030addc94
tehila97 <tehila284@gmail.com>
2024-10-14 13:33:00 +0300

    Add files via upload

---
+++ typeofOperator.js
@@ -0,0 +1,37 @@
+// ex 1
+
+const num = 10;
+const str = "Hello";
+const bool = true;
+
+console.log (typeof num);
+console.log (typeof str);
+console.log (typeof bool);
+
+// ex 2
+
+const firstVar = 42;
+const seconedVar ="42";
+
+console.log (typeof firstVar);
+console.log (typeof seconedVar);
+
+// ex 3
+
+const noValue = [];
+const emptyVariable = null;
+
+console.log (typeof noValue);
+console.log (typeof emptyVariable);
+
+// ex 4
+
+const X = "excellent";
+const Y = "WOW"
+const Z = 26;
+const L = true;
+
+console.log (`the value of X is ${X} and his type is ${typeof X}`);
+console.log (`the value of Y is ${Y} and his type is ${typeof Y}`);
+console.log (`the value of Z is ${Z} and his type is ${typeof Z}`);
+console.log (`the value of L is ${L} and his type is ${typeof L}`);
