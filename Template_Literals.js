e78b5c2d72fe013aa5a0e7c96767b32d94dc7693
tehila97 <tehila284@gmail.com>
2024-10-14 12:33:15 +0300

    Add files via upload

---
+++ Template_Literals.js
@@ -0,0 +1,37 @@
+// ex 1
+
+const name = "tehila";
+const age = 27;
+
+console.log (`my name is ${name} and i am ${age} years old`);
+
+// ex 2
+
+const length = 5;
+const width = 3;
+let multiple = length * width;
+
+console.log (`the area of a rectngular with length of ${length} and width of ${width} is ${multiple}`);
+
+// ex 3 
+const today = "Monday";
+const task_1 = "writing a letter";
+const task_2 = "eat lunch";
+const task_3 = "get a shawer";
+
+console.log (`on ${today} i'm planing to do: ${task_1}, ${task_2} and ${task_3}`);
+
+// ex 4
+
+const pets_i_have = 3;
+
+console.log (`i have ${pets_i_have} pets`);
+
+
+// ex 5
+
+const base = 5;
+const height = 4;
+const arae_of_traingle = (base*height) / 2;
+
+console.log (`the area of a trainangle with base ${base} and height of ${height} is ${arae_of_traingle}`)
