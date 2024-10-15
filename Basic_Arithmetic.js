97f8ff775888e71e4d50ca34f275377de22cb591
tehila97 <tehila284@gmail.com>
2024-10-15 14:54:50 +0300

    Add files via upload

---
+++ Basic_Arithmetic.js
@@ -0,0 +1,153 @@
+// // ex1
+
+// const num1 = 8;
+// const num2 = 15;
+// const sum = num1 + num2;
+
+// console.log (num1+num2);
+
+// // ex2
+
+// const a = 30;
+// const b = 12;
+
+// console.log (a-b);
+
+
+// // ex3
+// const X = 7;
+// const Y = 3;
+
+// console.log (X*Y);
+
+// // ex4
+
+// const dividend = 20;
+// const divisor = 4;
+
+// console.log (dividend/divisor);
+
+
+// // ex5
+
+// const number1 = 15;
+// const number2 = 25;
+// const number3 = 10;
+
+// console.log ((number1+number2+number3)/3);
+
+// // ex6
+
+// const varabile1 = 18;
+
+// console.log (varabile1 % 3);
+
+// // ex7 
+
+// const varabile2 = 20;  
+// console.log (varabile2 % 2);
+
+// const iseven = (varabile2 % 2 === 0);
+// console.log (iseven);
+
+
+// // ex8
+
+// const varabile3 = 20;
+
+// if (varabile3 % 5 === 0) {
+//     console.log(varabile3 + " is even");
+// }
+// else {
+//     console.log ("5 is odd");
+// }
+
+// if (varabile3 % 7 === 0) {
+//     console.log(varabile3 + " is even");
+// }
+// else {
+//     console.log (varabile3 + " is odd");
+// }
+
+// // ex9
+
+// for (let i=0; i <= 20; i++) {
+//     if (i % 4 === 0) {
+//         console.log (i);
+//     }
+//     else {
+//     }
+// }
+
+
+
+// ex10
+
+// const number1 = 100;
+// const number2 = 15;
+
+// if (number1 > 10 && number1 < 20 ) {
+//     console.log ("the number satisfying the condition")
+// }
+// else {
+//     console.log ("the number didn't satisfying the condition")
+// }
+
+// if (number2 > 10 && number2 < 20 ) {
+//     console.log ("the number satisfying the condition")
+// }
+// else {
+//     console.log ("the number didn't satisfying the condition")
+// }
+
+
+// ex11
+
+// const number1 = -2;
+// const number2 = 25;
+
+// if (number1 > -5 || number1 < 10) {
+//     console.log ("the number satisfying the condition")
+// }
+// else {
+//     console.log ("the number didn't satisfying the condition")
+// }
+
+// if (number2 > -5 || number2 < 10) {
+//     console.log ("the number satisfying the condition")
+// }
+// else {
+//     console.log ("the number didn't satisfying the condition")
+// }
+
+
+// ex12
+
+// const number1 = 25;
+
+// if  (!(number1 === 10)) {
+//     console.log (true);
+// }
+// else {
+//     console.log (false);
+// }
+
+
+// ex13
+
+const number1 = 100;
+const number2 = 8;
+
+if (number1 > 5 && number1 < 10 || number1 > 20 && number1 < 30 ) {
+        console.log ("the number satisfying the condition")
+}
+else {
+    console.log ("the number didn't satisfying the condition")
+}
+
+if (number2 > 5 && number2 < 10 || number2 > 20 && number2 < 30 ) {
+    console.log ("the number satisfying the condition")
+}
+else {
+console.log ("the number didn't satisfying the condition")
+}
