7f1b9b1a11a3f4799d4e23e53471ff3389b5f003
tehila97 <tehila284@gmail.com>
2024-10-25 01:06:10 +0300

    Add files via upload

---
+++ If-statements.js
@@ -0,0 +1,111 @@
+// ex 1
+
+// const trevelBudget = 500;
+
+// if (trevelBudget < 1000) {
+// console.log ("luxsery trevel options avalible");
+// }
+
+// else  {
+// console.log ("looking for budget trevel");
+// }
+
+// ex 2
+
+// const destinationTemp = 25;
+
+// if (destinationTemp < 27 && destinationTemp > 15) {
+//     console.log("destinationTemp ideal weather for the trip");
+// }
+// else {
+// console.log("consider another destination or time");
+// }
+
+
+// ex 3
+
+// const flightbook = false;
+// const hotelBooked = true;
+
+
+// if (flightbook === true) {
+//     if (hotelBooked === true) {
+//     console.log("your trevel plan is set");
+// }else if (hotelBooked === false) {
+//     console.log("you need to book a hotel");
+//     }
+// }else if (flightbook === false) {
+
+// console.log("you need to book a flight");
+// }
+
+// ex 4
+
+// const chosenPackagePrice = 100;
+// const alternativePackagePrice = 150;
+
+// if (chosenPackagePrice === alternativePackagePrice ) {
+// console.log("you have chosen the best priced packege");
+// }
+// else {
+// console.log('consider switching for a better price');
+// }
+
+
+// ex 5
+
+// const trevelAge = 30;
+
+// if (trevelAge >= 65) {
+// console.log("eligible for senior citizen discount");
+// }
+
+// else if (trevelAge <= 18) {
+//     console.log("eligible for student discount");
+// }
+
+// else {
+// console.log("reglar pricing applies");
+// }
+
+// ex 6
+
+// const stasifactionScore = 70;
+
+// stasifactionScore > 80 ? console.log ("highly stasfied traveler") : console.log ("needs improvment");
+
+
+// ex 7
+
+// const groupSize = 11;
+
+// groupSize % 2 === 0 ? console.log ("even numbers of travelers") : console.log("odd numbers of travelers");
+
+// ex 8
+
+// const trevelerAge = 25;
+
+// trevelerAge < 12 ? console.log ("child") :
+// trevelerAge > 13 && trevelerAge < 20 ? console.log ( "teen") :
+// trevelerAge > 20 ? console.log ( "adult"): console.log( "teen");
+
+// ex 9
+
+// const expirenceLevel = 3;
+
+// expirenceLevel < 5 ? console.log( "exprienced traveler"): console.log( "new traveler");
+
+// ex 10
+
+// const tripDuration = 14;
+
+// tripDuration < 3 ? console.log("Weekend getaway"):
+// tripDuration > 3 && tripDuration < 14 ? console.log( "standart vaction"):
+// console.log ("Extended tour");
+
+// ex 13
+
+// const destinationOpeningYear = 2099;
+
+// destinationOpeningYear > 2001 && destinationOpeningYear < 2100 ? console.log( "21st century destination") : 
+// console.log( "20st century destination") ;
